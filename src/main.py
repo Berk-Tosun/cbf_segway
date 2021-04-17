393110fd77d300f5f1937e0a41d354b8d9a2e6a8
Berk Tosun <berkistayn@gmail.com>
2021-04-18 00:07:26 +0300

    First movement of wheels

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import pybullet_data
 import time
 import pathlib
 
-dt = 1/25
+dt = 1e-3
 
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -15,17 +15,32 @@ p.loadURDF('plane.urdf')
 cubeStartPos = [0, 0, 1.13]
 cubeStartOrientation = p.getQuaternionFromEuler([0., 0, 0])
 urdf_file = pathlib.Path("./URDF/cyberpod.urdf")
-botId = p.loadURDF(urdf_file.absolute().as_posix(), cubeStartPos, 
+robot_id = p.loadURDF(urdf_file.absolute().as_posix(), cubeStartPos, 
     cubeStartOrientation)
 
 p.setGravity(0, 0, -9.807)
 p.setTimeStep(dt)
 
-# Run the simulation for a fixed amount of steps.
+joint_name2id = {}
+for i in range(p.getNumJoints(robot_id)):
+    joint_info = p.getJointInfo(robot_id, i)
+    joint_name2id[joint_info[1].decode('UTF-8')] = joint_info[0]
+
+target_angle = 0
+
 while p.isConnected():
-    position, orientation = p.getBasePositionAndOrientation(botId)
+    position, orientation = p.getBasePositionAndOrientation(robot_id)
     x, y, z = position
     roll, pitch, yaw = p.getEulerFromQuaternion(orientation)
+
+    target_angle = (target_angle + 0.001) % 3.1415
+    print(f"{target_angle=}")
+
+    p.setJointMotorControl2(robot_id, joint_name2id["w1_joint"],
+        p.POSITION_CONTROL, target_angle)
+    p.setJointMotorControl2(robot_id, joint_name2id["w2_joint"],
+        p.POSITION_CONTROL, target_angle)
+
     p.stepSimulation()
     time.sleep(dt)
 
